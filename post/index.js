73c0b14f953c8be0dba0c83a8d2f5b3b9806bee2
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-31 19:47:16 -0600

    added body parser

--- post/index.js
+++ post/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const { randomBytes } = require("crypto")
+const bodyParser = require("body-parser")
 
 const app = express()
+app.use(bodyParser.json())
 
 const posts = {}
 
@@ -11,6 +13,13 @@ app.get('/post', (req, res) => {
 
 app.post("/posts", (req, res) => {
     const id = randomBytes(4).toString("hex")
+    const { title } = req.body
+
+    post[id] = {
+        id, title
+    }
+
+    res.status(201).send(post[id])
 })
 
 app.listen(4000, () => {
